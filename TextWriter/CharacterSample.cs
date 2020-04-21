2b2045d2be6f6303719e3dd35c13c58f99674d78
super mario <mario160@mail.ru>
2020-04-21 13:12:45 +0300

    Interface improvements + character class created

---
+++ TextWriter/CharacterSample.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextWriter
+{
+    class CharacterSample
+    {
+        public Bitmap Img;
+        public Point Begin;
+        public Point End;
+
+        public const String ImgFormat = ".png";
+        public const String PointFromat = ".txt";
+
+        public CharacterSample(Bitmap image, Point begin, Point end)
+        {
+            Img = image;
+            Begin = begin;
+            End = end;
+        }
+        public CharacterSample(String folder, char character, int sampleInd)
+        {
+            if (folder[folder.Length - 1] != '\\') folder += "\\";
+            Img = (Bitmap)Image.FromFile(folder + character + sampleInd.ToString() + ImgFormat);
+            StreamReader reader = new StreamReader(folder + character + sampleInd.ToString() + PointFromat);
+            Begin = PointFromString(reader.ReadLine());
+            End = PointFromString(reader.ReadLine());
+            reader.Close();
+        }
+        public void Save(String folder, char character, int sampleInd)
+        {
+            if (folder[folder.Length - 1] != '\\') folder += "\\";
+            Img.Save(folder + character + sampleInd.ToString() + ImgFormat, 
+                System.Drawing.Imaging.ImageFormat.Png);
+            StreamWriter writer = new StreamWriter(folder + character
+                + sampleInd.ToString() + PointFromat);
+            writer.WriteLine(PointToString(Begin));
+            writer.WriteLine(PointToString(End));
+            writer.Close();
+        }
+
+        public static String PointToString(Point point)
+        {
+            return point.X.ToString() + ";" + point.Y.ToString();
+        }
+        public static Point PointFromString(String s)
+        {
+            String[] spl = s.Split(';');
+            return new Point(int.Parse(spl[0]), int.Parse(spl[1]));
+        }
+    }
+
+    class Character
+    {
+        public char Symbol;
+        public List<CharacterSample> Samples;
+
+        public Character(char symbol)
+        {
+            Symbol = symbol;
+            Samples = new List<CharacterSample>();
+        }
+        public Character(String folder, char symbol)
+        {
+            Symbol = symbol;
+            Samples = new List<CharacterSample>();
+            if (folder[folder.Length - 1] != '\\') folder += "\\";
+            for (int i = 0; File.Exists(folder + symbol + i.ToString() + CharacterSample.ImgFormat); i++)
+                Samples.Add(new CharacterSample(folder, symbol, i));
+        }
+        public void Save(String folder)
+        {
+            if (folder[folder.Length - 1] != '\\') folder += "\\";
+            for (int i = 0; i < Samples.Count; i++)
+                Samples[i].Save(folder, Symbol, i);
+        }
+
+        public void AddSample(CharacterSample sample)
+        {
+            Samples.Add(sample);
+        }
+    }
+}
--- TextWriter/MainWindow.xaml.cs
+++ TextWriter/MainWindow.xaml.cs
@@ -19,11 +19,34 @@ namespace TextWriter
 {
     public partial class MainWindow : Window
     {
-        BitmapImage bitmap;
+        Bitmap bitmap;
+        Bitmap buffBitmap;
+        Graphics graphics;
         OpenFileDialog open = new OpenFileDialog();
+        double scaleFactor = 300.0;
+
+        const String NeededChars = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюяАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ.-!?:,«»";
+        List<Character> Characters;
         public MainWindow()
         {
             InitializeComponent();
+            Characters = new List<Character>();
+        }
+        int checkIfAllCharactersAdded()
+        {
+            if (Characters.Count < NeededChars.Length) 
+                return Characters.Count;
+            else return -1;
+        }
+        BitmapSource fromBitmap(Bitmap bitmap)
+        {
+            return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    bitmap.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty,
+                    BitmapSizeOptions.FromWidthAndHeight(bitmap.Width, bitmap.Height));
+        }
+        void onBitmapChanged()
+        {
+            Preview.Source = fromBitmap(bitmap);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -32,8 +55,12 @@ namespace TextWriter
             open.Filter = "Image files|*.bmp;*.jpg;*.gif;*.png;*.tif";
             if(open.ShowDialog() == true)
             {
-                bitmap = new BitmapImage(new Uri(open.FileName));
-                Preview.Source = bitmap;
+                bitmap = (Bitmap)Image.FromFile(open.FileName);
+                buffBitmap = bitmap;
+                onBitmapChanged();
+                graphics = Graphics.FromImage(bitmap);
+
+                
             }
         }
 
@@ -46,23 +73,19 @@ namespace TextWriter
             }
             else if (Keyboard.IsKeyDown(Key.LeftCtrl))
             {
-                PreviewScale.ScaleX += e.Delta/100;
-                PreviewScale.ScaleY += e.Delta/100;
+                double k = Math.Pow(2, (double)e.Delta / scaleFactor);
+                PreviewScale.ScaleX *= k;
+                PreviewScale.ScaleY *= k;
                 e.Handled = true;
             }
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(Scroller.HorizontalOffset.ToString());
         }
 
         System.Windows.Point convertToPixels(System.Windows.Point p)
         {
             System.Windows.Point ret = p;
-            ret.X *= bitmap.PixelWidth;
+            ret.X *= bitmap.Width;
             ret.X /= Preview.ActualWidth;
-            ret.Y *= bitmap.PixelHeight;
+            ret.Y *= bitmap.Height;
             ret.Y /= Preview.ActualHeight;
             return ret;
         }
@@ -70,15 +93,56 @@ namespace TextWriter
         private void Preview_MouseDown(object sender, MouseButtonEventArgs e)
         {
             System.Windows.Point pos = convertToPixels(e.GetPosition(Preview));
-            double x = pos.X;
-            double y = pos.Y;
+            int x = (int)pos.X;
+            int y = (int)pos.Y;
 
-            int n_x = bitmap.PixelWidth * 4;
-            int siz = n_x * bitmap.PixelHeight;
-            byte[] pixels = new byte[siz];
-            bitmap.CopyPixels(pixels, n_x, 0);
-            int i = (int)y * n_x + (int)x * 4;
-            MessageBox.Show(pixels[i] + " " + pixels[i+1] + " " + pixels[i+2]);
+            graphics.FillEllipse(System.Drawing.Brushes.White, x, y, 50, 50);
+            onBitmapChanged();
+        }
+
+        private void add_character_button_Click(object sender, RoutedEventArgs e)
+        {
+            int res = checkIfAllCharactersAdded();
+            if (res == -1) MessageBox.Show("You have added all characters");
+            else if (buffBitmap != null)
+            {
+                Character newC = new Character(NeededChars[res]);
+                newC.AddSample(new CharacterSample(null,
+                    new System.Drawing.Point(0, 0), new System.Drawing.Point(0, 0)));
+                Characters.Add(newC);
+                updateLists();
+            }
+            else
+                MessageBox.Show("Firstly open image.", "Error", 
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        void updateLists()
+        {
+            if (Characters.Count > characters_listbox.Items.Count)
+            {
+                characters_listbox.Items.Clear();
+                for (int i = 0; i < Characters.Count; i++)
+                    characters_listbox.Items.Add(Characters[i].Symbol);
+                characters_listbox.SelectedIndex = Characters.Count - 1;
+            }
+            else
+                characters_listbox_SelectionChanged(null, null);
+        }
+
+        private void variants_listbox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void characters_listbox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if(characters_listbox.Items.Count > 0)
+            {
+                Character selected = Characters[characters_listbox.SelectedIndex];
+                variants_listbox.Items.Clear();
+                for (int i = 0; i < selected.Samples.Count; i++)
+                    variants_listbox.Items.Add(i);
+            }
         }
     }
 }
